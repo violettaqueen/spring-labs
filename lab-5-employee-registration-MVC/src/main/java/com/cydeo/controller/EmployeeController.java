c94964d8c0f34bd4f80b0f025b8e04c46900d110
violettaqueen <violetta.v.queen@gmail.com>
2022-09-27 10:30:29 -0600

    html is finished

--- lab-5-employee-registration-MVC/src/main/java/com/cydeo/controller/EmployeeController.java
+++ lab-5-employee-registration-MVC/src/main/java/com/cydeo/controller/EmployeeController.java
@@ -1,6 +1,7 @@
 package com.cydeo.controller;
 
 import com.cydeo.bootstrap.DataGenerator;
+import com.cydeo.model.Employee;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,8 +11,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/employee")
 public class EmployeeController {
     @GetMapping("/register")
-    public String createEmployee(){
+    public String createEmployee(Model model){
 
+        model.addAttribute("employee", new Employee());
         return "employee/employee-create";
     }
 }
