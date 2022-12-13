f6fd4b0d6df011d43099c5ee1e5a6bdea257f0ff
Violetta <violetta.v.queen@gmail.com>
2022-12-13 13:45:22 -0700

    lab-08-REST_cascading added

--- lab-08-rest/src/main/java/com/cydeo/entity/Order.java
+++ lab-08-rest/src/main/java/com/cydeo/entity/Order.java
@@ -3,10 +3,7 @@ package com.cydeo.entity;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.math.BigDecimal;
 
 @Entity
@@ -20,6 +17,6 @@ public class Order extends BaseEntity{
     private BigDecimal totalPrice;
     @ManyToOne
     private Customer customer;
-    @OneToOne
+    @OneToOne(cascade = CascadeType.ALL)
     private Payment payment;
 }
